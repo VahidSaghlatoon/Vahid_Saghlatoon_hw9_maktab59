8c0b4c2f8d1b255b57443e00a42194adf1191e6b
vahid <vahid.d.w@gmail.com>
2021-10-05 15:24:29 +0330

    change

--- src/com/vahidsaghlatoon/TreeSetQ.java
+++ src/com/vahidsaghlatoon/TreeSetQ.java
@@ -2,29 +2,30 @@ package com.vahidsaghlatoon;
 
 import java.util.Random;
 import java.util.Set;
+import java.util.TreeSet;
 
-public class TreeSet {
+public class TreeSetQ {
 
 
     public static void main(String[] args) {
         //new object from treeSetEx class
-        TreeSet treeSet = new TreeSet();
+        TreeSetQ treeSetQ = new TreeSetQ();
         //new treeSet
-        Set<Character> treeSet1 = new java.util.TreeSet();
-        treeSet1 = treeSet.SetRandomChars(treeSet1);
-        treeSet.printTreeSetElements("Tree set 1", treeSet1);
+        Set<Character> treeSet1 = new TreeSet<>();
+        treeSet1 = treeSetQ.SetRandomChars(treeSet1);
+        treeSetQ.printTreeSetElements("Tree set 1", treeSet1);
         //new treeSet
-        Set<Character> treeSet2 = new java.util.TreeSet();
-        treeSet2 = treeSet.SetRandomChars(treeSet2);
-        treeSet.printTreeSetElements("Tree set 2", treeSet2);
+        Set<Character> treeSet2 = new TreeSet<>();
+        treeSet2 = treeSetQ.SetRandomChars(treeSet2);
+        treeSetQ.printTreeSetElements("Tree set 2", treeSet2);
 
         Set<Character> combinationTreeSets ;
-        combinationTreeSets = treeSet.combination(treeSet1, treeSet2);
-        treeSet.printTreeSetElements("combination of treeSet 1 and 2",combinationTreeSets);
+        combinationTreeSets = treeSetQ.combination(treeSet1, treeSet2);
+        treeSetQ.printTreeSetElements("combination of treeSet 1 and 2",combinationTreeSets);
 
         Set<Character> share ;
-        share = treeSet.share(treeSet1, treeSet2);
-        treeSet.printTreeSetElements("share of treeSet 1 and 2",share);
+        share = treeSetQ.share(treeSet1, treeSet2);
+        treeSetQ.printTreeSetElements("share of treeSet 1 and 2",share);
     }
 
     public Set<Character> SetRandomChars(Set<Character> treeSet) {
@@ -46,12 +47,12 @@ public class TreeSet {
     }
 
     public Set<Character> combination(Set<Character> treeSet1, Set<Character> treeSet2) {
-        return new java.util.TreeSet(){{addAll(treeSet1); addAll(treeSet2);}};
+        return new TreeSet<>(){{addAll(treeSet1); addAll(treeSet2);}};
     }
 
     public Set<Character> share(Set<Character> treeSet1, Set<Character> treeSet2)
     {
-        Set<Character> share = new java.util.TreeSet();
+        Set<Character> share = new TreeSet<>();
         for (char newChar : treeSet1) {
             if (treeSet2.contains(newChar))
                 share.add(newChar);
